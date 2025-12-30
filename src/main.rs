e8b92d2c2e986e68a2ceebffafc2df1a3b3db637
AliA12336 <amadams12336@gmail.com>
2025-12-29 19:11:50 -0500

    Add post endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::sync::{Arc, Mutex};
 
 use axum::{
-    Json, Router, extract::{State}, response::IntoResponse, routing::get
+    Error, Json, Router, extract::State, response::IntoResponse, routing::get
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 
 #[derive(Clone, Serialize)]
@@ -15,13 +15,14 @@ struct Book {
 
 type Books = Arc<Mutex<Vec<Book>>>;
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Serialize, Deserialize)]
 enum Status {
     Reading,
     Completed, 
     Soon,
 }
 
+#[derive(Deserialize)]
 struct PostBooks {
     title: String,
     status: Status,
@@ -34,13 +35,25 @@ async fn get_books(State(books): State<Books>) -> impl IntoResponse {
     Json(book_lock.clone())
 }
 
-async fn post_books_handler(State(books): State<Books>, Json(payload): Json<PostBooks>) {}
+#[axum::debug_handler]
+async fn post_book_handler(State(books): State<Books>, Json(payload): Json<PostBooks>) -> Json<Book> {
+    let mut book_lock = books.lock().unwrap();
+    let book_id = book_lock.len() + 1;
+    let new_book = Book {
+        id: book_id,
+        title: payload.title,
+        status: payload.status,
+    };
+
+    book_lock.push(new_book.clone());
+    Json(new_book)
+}
 
 #[tokio::main]
 async fn main() {
     let state = Arc::new(Mutex::new(Vec::<Book>::new()));
     // build our application with a single route
-    let app = Router::new().route("/books", get(get_books)).with_state(state);
+    let app = Router::new().route("/books", get(get_books).post(post_book_handler)).with_state(state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
